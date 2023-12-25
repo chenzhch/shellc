f14ab751343a299616861abe2876c206ccbf960b
chenzhch <nbczc@sina.com>
2023-12-25 22:08:56 +0800

    add untraceable

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ cc -s shellc.c -O2 -o shellc
 ### Compile shell scripts and generate executable programs
 ```bash
 shellc example.sh
-cc -s example.sh.c -O2 -o example
+cc -s example.sh.c -O2 -o example -lpthread
 ```
 The generated C code is ```example.sh.c```, which is the original file name followed by ```.c```.
 For programs used in production environments, it is recommended to add the parameter ```-s``` when compiling to remove the symbol table in the executable program, increasing the difficulty of disassembly and reverse engineering.
@@ -16,13 +16,39 @@ I have tried the tools ```shc``` and ```shellcrypt```, which can compile shell s
 - The ```shc``` command actually calls the ```sh -c``` command.  The source code can be seen through the ```ps -ef``` command. The length of the shell script cannot exceed ARG_ MAX .
 - ```Shellcrypt``` does not support external parameter input, and the encryption algorithm is relatively simple.  The generated executable program can be decrypted by XORing with ```0x000C``` to obtain the source code.
 
-In addition to solving the problems of the above two tools, ```shellc``` also adds code obfuscation and randomly generates effective character position calculation functions to increase the complexity of disassembly and reverse engineering.If further difficulty is required, professional tools such as ```obfuscator-llvm``` can be used to further obfuscate the generated C code.
+In addition to solving the problems of the above two tools, ```shellc```  also adds code obfuscation, randomly generating effective character position calculation functions, random character encryption, and debugging to increase the complexity of disassembly and reverse engineering.
 
-This program is suitable for shells that can define function syntax, such as ```ksh```, ```bsh```, ```zsh```, etc., and does not support ```csh``` (```csh``` does not support function definition).Currently, the program has been tested and has practical applications in ```AIX7.1```, ```SCO UNIX5.0.6```, ```Red Hat Linux7.8```, and ```CentOS Linux6.4``` environments.It is recommended to conduct comprehensive testing on the compiled program to prevent production failures.
+This program is suitable for shells that can define function syntax, such as ```ksh```, ```bsh```, ```zsh```, etc., and does not support ```csh``` (```csh``` does not support function definition).Currently, the program has been tested and has practical applications in ```AIX```, ```UNIX```, and ```Linux``` environments.It is recommended to conduct comprehensive testing on the compiled program to prevent production failures.
 
 **If the shell script involves human-computer interaction, some modifications need to be made to the original shell script, and ```read input``` needs to be changed to ```read input </dev/tty```.**
 
+Version 3 has added untraceable and tested the following tools for untraceable
+
+OS| TOOL|SUPPORTED
+------|------|------
+AIX 7.1|dbx sdb|YES
+SCO UNIX 5.0.6|dbx sdb adb|YES
+Red Hat Linux7.8|gdb|YES
+CentOS Linux6.4|gdb|YES
+debian 12.4.0|gdb|YES
+Fedora 39|gdb lldb|YES
+FreeBSD 14.0|gdb|YES
+openSUSE Leap 15.5|gdb|YES
+OracleLinux R9|gdb|YES
+ubuntu 22.04.3|gdb lldb|YES
+NetBSD 10.0|gdb|NO
+DragonFly 6.4|gdb|NO
+macOS 13|lldb|NO
+
+**For systems that do not support anti debugging functionality, the generated code needs to use the - t option. BSD and LINUX variant systems require thread support to enable anti debugging functionality**
+
 ### History
+- v0.3 2023-12-25
+
+  Add untraceable
+
+  Fix compilation warnings in some systems
+
 - v0.21 2023-12-13
 
   Fix program bugs in the ```zsh``` environment
--- shellc.c
+++ shellc.c
@@ -4,16 +4,18 @@
  * Function: Convert shell scripts into C code
  * Author: ChenZhongChao
  * E-Mail: nbczc@163.com
- * Date: 2023-12-12
- * Version: 0.2
+ * Date: 2023-12-25
+ * Version: 0.3
  * Github: https://github.com/chenzhch/shellc.git
  * Gitee: https://gitee.com/chenzhch/shellc.git
  */
 
-#include <sys/stat.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <time.h>
+#include <sys/utsname.h>
+#include <sys/stat.h>
 
 #define F1(a, b, c, d)  ((~c|~a)>(~d|~b)?b-c:a-d)
 #define F2(a, b, c, d)  ((~a|~c)>(~b|~d)?a<<c:b<<d)
@@ -59,11 +61,15 @@ static const char *head[] = {
     "#include <stdio.h>",
     "#include <string.h>",
     "#include <stdlib.h>",
-    "",
+    "#include <time.h>",
+    "#include <signal.h>",
+    "#include <unistd.h>",
+    "#include <sys/types.h>",
+    "#include <sys/wait.h>",
     0
 };
 
-static const char *middle[] = {
+static const char *tail[] = {
     "typedef long(*Fn)(long, long);",
     "",
     "long ff(Fn fn, long a, long b)",
@@ -71,63 +77,140 @@ static const char *middle[] = {
     "    return fn(a, b);",
     "}",
     "",
-    "void main(int argc, char **argv)",
+    "typedef struct {",
+    "    int argc;",
+    "    char **argv;",
+    "} Param;",
+    "",
+    "int handler(int argc, char **argv)",
     "{",
-    "    int i, j;",
+    "    int i, j, k;",
     "    FILE *pipe;",
     "    long mask;",
-    "    char str[3], chr[2];",
+    "    char chr[3], *str;",
     "    Fn fn[] = {",
     "        f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16,",
-    "        f17, f18, f19, f20, f21, f22, f23, f24, f25, f26, f27, f28, f29, f30, f31, f32",
+    "        f17, f18, f19, f20, f21, f22, f23, f24, f25, f26, f27, f28, f29, f30, f31, f32,",
+    "        f33, f34, f35, f36, f37, f38, f39, f40, f41, f42, f43, f44, f45, f46, f47, f48,",
+    "        f49, f50, f51, f52, f53, f54, f55, f56, f57, f58, f59, f60, f61, f62, f63, f64",
     "    };",
-    "    char *main = \"____shellC___MaiN__fUNcTioN_20231212_v02_\";",
-    0
-};
-
-static const char *tail[] = {
     "    if(getenv(\"SHELL\") == NULL) {",
     "        printf(\"Failed to get shell environment variable\\n\");",
-    "        exit(1);",
+    "        return(1);",
     "    }",
     "    pipe = popen(getenv(\"SHELL\"), \"w\");",
     "    if(pipe == NULL) {",
     "        printf(\"Failed to popen shell\\n\");",
-    "        exit(1);",
+    "        return(1);",
     "    }",
-    "    fputs(main, pipe);",
-    "    fputs(\"(){ \", pipe);",
+    "    srand(time(0));",
     "    i = 0;",
-    "    memset(str, 0, sizeof(str));",
-    "    memset(chr, 0, sizeof(chr));",
+    "    memset(chr, 0, (size_t) sizeof(chr));",
     "    while(data[i]) {",
     "        mask = (long) 1 << (atol(data[i + 1]) - 1);",
-    "        for(j = 0; j < atol(data[i + 1]); j++) {",
-    "            memcpy(str, &data[i + 5][j * 2], 2);",
-    "            sprintf(chr, \"%c\",",
-    "               (atol(data[i + 2]) + ff(fn[atol(data[i]) -1], atol(data[i + 3]), atol(data[i + 4]))) & mask", 
-    "               ? strtol(str, 0, 16) ^ ((atol(data[i + 2]) + ff(fn[atol(data[i]) -1], atol(data[i + 3]), atol(data[i + 4]))) % 256)", 
+    "        k = rand() % 8 + 2;",
+    "        str = (char *) malloc(k);",
+    "        memset(str, 0, (size_t) k);",
+    "        for(j = 0; j < atol(data[i + 1]); j++) {",        
+    "           memcpy(chr, &data[i + 5][j * 2], 2);",            
+    "           sprintf(",
+    "               str + strlen(str), \"%c\",",
+    "               (atol(data[i + 2]) + ff(fn[atol(data[i]) -1], atol(data[i + 3]), atol(data[i + 4]))) & mask",
+    "               ? (int) strtol(chr, 0, 16)",
+    "               ^ (int) (atol(data[i + 2]) + ff(fn[atol(data[i]) -1], atol(data[i + 3]), atol(data[i + 4]))) % 256",
+    "               ^ (int) ff(fn[j % 32 + 32], atol(data[i + 3]), atol(data[i + 4])) % 256",
     "               : 0",
-    "            );",
-    "            fputs(chr, pipe);",
-    "            mask >>= 1;",
-    "        }",
-    "        i += 6;",
+    "           );",  
+    "           if(strlen(str) == k - 1) {",
+    "               fputs(str, pipe);",
+    "               memset(str, 0, (size_t) k);",
+    "           }",            
+    "           mask >>= 1;",
+    "       }",
+    "       if(strlen(str) != 0) {",
+    "           fputs(str, pipe);",
+    "       }",
+    "       free(str);",
+    "       i += 6;",
     "    }",
-    "    fputs(\"\\n}\\n\", pipe);",
-    "    fputs(main, pipe);",
     "    for(i = 1; i < argc; i++) {",
     "        fputs(\" \\\"\", pipe);",
     "        fputs(argv[i], pipe);",
     "        fputc('\"', pipe);",
     "    }",
-    "    i = pclose(pipe);",
-    "    if(i != 0) {",
-    "        exit(1);",
+    "    if(pclose(pipe)) {",
+    "        return(1);",
     "    }",
-    "    exit(0);",
+    "    return(0);",
+    "}",  
+    "",
+    "int process(void *arg)", 
+    "{",
+    "    Param *param = (Param *) arg;",
+    "    return(handler(param->argc, param->argv));",
+    "}",
+    "",
+    0
+}; 
+
+static const char *main_func[] = {
+    "int main(int argc, char **argv)",
+    "{",   
+    0,
+};
+
+static const char *ptrace_sco[] = {
+    "    if(ptrace(0, getppid(), 0, 0)) {",
+    "        return(1);",
+    "    } else {",
+    "        kill(getppid(), SIGCONT);",
+    "    }",
+    "    return(handler(argc, argv));",
     "}",
     0
+};
+
+static const char *ptrace_aix[] = {
+    "    pid_t pid = 0;",
+    "    if(fork() == 0) {",
+    "        pid = getpid();",
+    "        if(__linux_ptrace(PT_WRITE_GPR, getppid(), 0, 0)) {",
+    "            kill(getppid(), SIGKILL);",
+    "        } else {",
+    "            __linux_ptrace(PT_WRITE_FPR, getppid(), 0, 0);",
+    "        }",
+    "        return(0);",
+    "    }",
+    "    waitpid(pid, 0, 0);",
+    "    return(handler(argc, argv));",
+    "}",
+    0
+};
+
+static const char *ptrace_linux[] = {
+    "    Param param = {argc, argv};",
+    "    pthread_t id = 0;",
+    "    void *result;",
+    "    if(ptrace(0, 0, 0, 0)) {",
+    "        return(1);",        
+    "    }",
+    "    if(pthread_create(&id, 0, (void *) process, &param)) {",
+    "        perror(\"Faild to pthread_create\");",
+    "        return(1);",
+    "    }",
+    "    if(pthread_join(id, &result)) {",
+    "        perror(\"Faild to pthread_join\");",
+    "        return(1);",
+    "    }",
+    "    return((long) result);",
+    "}",
+    0 
+};
+
+static const char *traced[] = {
+    "    return(handler(argc, argv));",
+    "}",
+    0        
 };
 
 /*Binary characters to decimal numbers*/
@@ -182,39 +265,74 @@ void macro(int order, int x, int y, char *str)
     }
 }
 
-void main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-    struct stat status;
-    FILE *in, *out, *pipe;
-    int code_length, obscure_length, length;
-    unsigned char *code_text, *obscure_text;
-    unsigned char *bitmap, *text;
-    unsigned char str[64];
+    FILE *in, *out;
+    int code_length, obscure_length, length, pos;
+    char *code_text, *obscure_text, *text;
+    char *bitmap, *inname, *outname;
+    char str[64];
     long result, offset1, offset2;
-    long mask, salt;
+    long salt1, salt2;
     int size, digit;
     int route[32][17], x[32][16], y[32][16];
-    int i, j, k, ch, loop, mode;
-    if(argc != 2) {
-        printf("Usage: %s file\n", argv[0]);
-        exit(1);
+    char *shellc = "____sHeLl___C__MaiN_fUNcTioN___";
+    char *operators = "+-*/%&|^";
+    char number[32][32][8];
+    char algorithm[32][17];
+    int i, j, k, loop, mode;
+    int flag = 0;    
+    struct utsname sysinfo;
+    struct stat status;  
+    
+    if(argc != 2 && argc != 3) {
+        printf("Usage: %s [-t] infile\n", argv[0]);
+        return(0);
     }
-    in = fopen(argv[1], "r");
-    if (in == NULL) {
-        printf("Failed to open the file %s\n", argv[1]);
-        exit(1);
-    }
-    stat(argv[1], &status);
-    code_length = status.st_size;
-    code_text = (unsigned char *) malloc(code_length);
-    for(i = 0; i < code_length; i++) {
-        code_text[i] = fgetc(in);
+    if(argc == 2 && !strcmp("-h", argv[1])) {
+        printf("%s: Convert shell scripts into C code\n", argv[0]);
+        printf("Usage: %s [-t] infile\n", argv[0]);
+        printf("Option: \n");
+        printf("    -t    Make traceable binary program\n");
+        printf("    -h    Display help and return\n");
+        return(0);
+    }else if(argc == 2 && !strcmp("-t", argv[1])) {
+        printf("Usage: %s [-t] infile\n", argv[0]);
+        return(0);
+    }else if(argc == 3) {
+        if(strcmp("-t", argv[1]) && strcmp("-t", argv[2])) {
+            printf("Usage: %s [-t] infile\n", argv[0]);
+            return(0);    
+        }
+        if(!strcmp("-t", argv[1])) {
+            inname = strdup(argv[2]); 
+        }else {
+            inname = strdup(argv[1]);    
+        }
+        flag = 1;                
+    }else {
+        inname =  strdup(argv[1]);
     }
     
+    in = fopen(inname, "r");
+    if (in == NULL) {
+        printf("Failed to open the file: %s\n", inname);
+        return(1);
+    }
+    stat(inname, &status);
+    code_length = status.st_size + strlen(shellc) * 2 + 32;
+    code_text = (char *) malloc(code_length);
+    memset(code_text, 0, (size_t) code_length);
+    sprintf(code_text, "%s(){ ", shellc);
+    i = strlen(code_text);
+    for(j=0; j < status.st_size; j++) {
+        code_text[i++] = fgetc(in);
+    }
+    sprintf(code_text + strlen(code_text), "%c}%c%s", '\n', '\n', shellc);
     srand(time(0));
-    obscure_length = code_length / 5 * 6  + rand() % 1024 + 4096;
-    bitmap = (unsigned char *) malloc(obscure_length * 2);
-    obscure_text = (unsigned char *) malloc(obscure_length * 2);
+    obscure_length = code_length / 5 * 6  + rand() % 1024 + 4096; 
+    bitmap = (char *) malloc(obscure_length * 2);
+    obscure_text = (char *) malloc(obscure_length * 2);
     
     /*Randomly generated visible obfuscated characters*/
     loop = 1;
@@ -236,11 +354,10 @@ void main(int argc, char **argv)
             loop = 0;
         }
     }
-
     /*Total length after confusion*/
     obscure_length = j;
-
-    /*Generate calculation factors*/
+ 
+    /*Effective location calculation factors*/
     for(i= 0; i < 32; i++) {
         for(j = 0; j < 16; j++){
             x[i][j] = rand() % 10+1;
@@ -248,7 +365,7 @@ void main(int argc, char **argv)
         }
     }
 
-    /*Generate a computation path*/
+    /*Effective location calculation path*/
     for(i = 0; i < 32; i++) {
         k = rand() % 9 + 8;
         for(j = 0; j < k; j++) {
@@ -256,19 +373,39 @@ void main(int argc, char **argv)
         }
         route[i][j]=0;
     }
-
-    text = (char *) malloc(strlen(argv[1]) + 3);
-    memset(text, 0, sizeof(text));
-    sprintf(text, "%s.c", argv[1]);
-    out = fopen(text, "w");
-    if (out == NULL) {
-        printf("Failed to write to file %s\n", text);
-        exit(1);
+    
+    /*Character mask calculation path*/
+    for(i= 0; i < 32; i++) {
+        k = rand() % 9 + 8; 
+        for(j = 0; j < k; j++){
+            algorithm[i][j] = operators[rand() % 8];
+            sprintf(number[i][j * 2], "%c", 'a' + rand() % 2);
+            sprintf(number[i][j * 2 + 1], "%d", rand() % 1000 + 10);
+        }
+        algorithm[i][j] = 0;
     }
-
+    
+    outname = (char *) malloc(strlen(inname) + 3);
+    sprintf(outname, "%s.c", inname);
+    out = fopen(outname, "w");
+    if (out == NULL) {
+        printf("Failed to write to file %s\n", outname);
+        return(1);
+    }
+    
     /*Write to the file header*/
-    for(i=0; head[i]; i++) {
-        fprintf(out, "%s\n", head[i]);
+    i = 0;
+    while(head[i]) fprintf(out, "%s\n", head[i++]);
+        
+    if(!flag) {
+        if (uname(&sysinfo)) {
+            perror("Failed to uname");
+            return(1);
+        }  
+        if(strcmp(sysinfo.sysname, "SCO_SV")) {
+            fprintf(out, "#include <pthread.h>\n");
+            fprintf(out, "#include <sys/ptrace.h>\n");
+        }    
     }
 
     /*Macro definitions are generated based on the calculation path*/
@@ -278,29 +415,38 @@ void main(int argc, char **argv)
             if(j == 0){
                 fprintf(out, "#define F%d(a, b) (\\\n", i + 1);
             }
-            memset(str, 0, sizeof(str));
+            memset(str, 0, (size_t) sizeof(str));
             macro(route[i][j], x[i][j], y[i][j], str);
             fprintf(out, "   %s%s\\\n", j == 0 ? " ":" +", str);
             j++;
         }
         fprintf(out, ")\n\n");
     }
+    
+    /*Character mask calculation function*/
+    for(i = 0; i < 32; i++) {
+        j = 0;
+        while(algorithm[i][j] != 0) {
+            if(j == 0){
+                fprintf(out, "#define F%d(a, b) labs(\\\n", 32 + i + 1);
+            }
+            fprintf(out, "   %s(%s%c%s)\\\n", j == 0 ? " ":" +", number[i][j * 2], algorithm[i][j], number[i][j * 2 + 1]);
+            j++;
+        }
+        fprintf(out, ")\n\n");
+    }
 
     /*Generate a call function*/
-    for(i = 0; i < 32; i++) {
+    for(i = 0; i < 64; i++) {
         fprintf(out, "long f%d(long a, long b)\n", i + 1);
         fprintf(out, "{\n");
         fprintf(out, "    return(F%d(a, b));\n", i + 1);
         fprintf(out, "}\n\n");
-    }
-
-    /*Write to the main function section*/
-    for(i=0; middle[i]; i++) {
-        fprintf(out, "%s\n", middle[i]);
-    }
+    }        
     
-    fprintf(out, "    char *data[] = {\n");
-    free(text);
+    /*Write to the data section*/        
+    fprintf(out, "static const char *data[] = {\n");
+   
     size = sizeof(long) * 8;
     text =(char *) malloc(size);
     j = 0;
@@ -312,9 +458,9 @@ void main(int argc, char **argv)
             length = obscure_length;
         }
         obscure_length -= length;
-        memset(text, 0, sizeof(text));
+        memset(text, 0, (size_t) size);
         result = binary_to_int(bitmap + j, length);
-        salt = result % 256;
+        salt1 = result % 256;
         memcpy(text, obscure_text + j, length);
         j += length;
         offset1 = rand() % 1000 + 10;
@@ -357,24 +503,60 @@ void main(int argc, char **argv)
             }
             i++;
         }
-        fprintf(out, "        \"%d\", \"%d\", \"%ld\", \"%ld\", \"%ld\", \n        \"", mode + 1, length, result, offset1, offset2);
+        fprintf(out, "    \"%d\", \"%d\", \"%ld\", \"%ld\", \"%ld\", \n    \"", mode + 1, length, result, offset1, offset2);
+        
         for(i = 0; i < length; i++) {
-            fprintf(out, "%02X", ((unsigned char) text[i]) ^ salt);
+            salt2 = 0;
+            k = 0;
+            pos = i % 32;
+            while(algorithm[pos][k]){
+                switch(algorithm[pos][k]){   
+                    case '+': salt2 += (number[pos][k * 2][0] == 'a' ? offset1 : offset2) + atol(number[pos][k * 2 + 1]); break;
+                    case '-': salt2 += (number[pos][k * 2][0] == 'a' ? offset1 : offset2) - atol(number[pos][k * 2 + 1]); break; 
+                    case '*': salt2 += (number[pos][k * 2][0] == 'a' ? offset1 : offset2) * atol(number[pos][k * 2 + 1]); break;
+                    case '/': salt2 += (number[pos][k * 2][0] == 'a' ? offset1 : offset2) / atol(number[pos][k * 2 + 1]); break;
+                    case '%': salt2 += (number[pos][k * 2][0] == 'a' ? offset1 : offset2) % atol(number[pos][k * 2 + 1]); break;       
+                    case '&': salt2 += (number[pos][k * 2][0] == 'a' ? offset1 : offset2) & atol(number[pos][k * 2 + 1]); break;          
+                    case '|': salt2 += (number[pos][k * 2][0] == 'a' ? offset1 : offset2) | atol(number[pos][k * 2 + 1]); break;    
+                    case '^': salt2 += (number[pos][k * 2][0] == 'a' ? offset1 : offset2) ^ atol(number[pos][k * 2 + 1]); break; 
+                } 
+                k++;
+            }
+            salt2 = labs(salt2) % 256;
+            fprintf(out, "%02X", ((unsigned char) text[i]) ^ ((int) salt1) ^ ((int) salt2));
         }
         fprintf(out, "\",\n");
     }
-    fprintf(out, "        0\n");
-    fprintf(out, "    };\n");
+    fprintf(out, "    0\n");
+    fprintf(out, "};\n\n");   
+   
+    i = 0;
+    while(tail[i]) fprintf(out, "%s\n", tail[i++]);
+   
+    /*Write to the main function section*/
+    i = 0;
+    while(main_func[i]) fprintf(out, "%s\n", main_func[i++]);    
 
-    /*Write to the end of the file*/
-    for(i=0; tail[i]; i++) {
-        fprintf(out, "%s\n", tail[i]);
+    i = 0;
+    if(flag) {      
+        while(traced[i]) fprintf(out, "%s\n", traced[i++]);        
+    } else if (!strcmp(sysinfo.sysname, "SCO_SV")){
+        while(ptrace_sco[i]) fprintf(out, "%s\n", ptrace_sco[i++]);          
+    } else if (!strcmp(sysinfo.sysname, "AIX")){
+        while(ptrace_aix[i]) fprintf(out, "%s\n", ptrace_aix[i++]);        
+    } else {
+        while(ptrace_linux[i]) fprintf(out, "%s\n", ptrace_linux[i++]); 
+        flag = 2;            
     }
-
+   
     fflush(out);
     fclose(out);
+    printf("Ok! You can make binary program by \"cc %s -s -O2 -o %s.x%s\"\n", outname, inname, flag == 2 ? " -lpthread" : "");
     free(bitmap);
     free(code_text);
     free(obscure_text);
     free(text);
+    free(inname);
+    free(outname);
+    return(0);
 }
